1883d0cb0d873b7d18836fa43c56222499fc6219
AngelDOrtiz <aortizghs@gmail.com>
2021-04-13 11:26:10 -0700

    :)

---
+++ Item-Masterlist.js
@@ -0,0 +1,33 @@
+ /* {
+  'name':'Name',
+  'id': 'ID num',
+  'type': 'Type',
+  'level': 'Level num',
+  'traits : ['Trait', 'Trait'],
+  'rarity': 'Rarity',
+  'description': 'Description'
+}, */
+
+
+export const magicItems = [
+{
+  'name': 'Ring of Mimic Cry',
+  'id': 1,
+  'type': 'ring',
+  'level': 5,
+  'traits': ['Divination', 'Invested', 'Magical'],
+  'rarity': 'Uncommon',
+  'description': 'A polished wooden ring with metal bands inlaid; a small image of a lock burned into the face of the ring. While wearing the ring, you may cast the Locate spell at 3rd level once per week as a spontaneous occult spell, but only to find Mimics.'
+},
+
+{
+  'name': 'Bautor',
+  'id': 2,
+  'type': 'weapon',
+  'level': 12,
+  'traits' : ['Invested', 'Magical', 'Necromancy', 'Transmutation'],
+  'rarity': 'Unique',
+  'description': 'The entirety of the blade of this +2 Greater Striking Wounding Longsword is made of a crimson, polished crystalline material; its hilt made of cold iron that perpetually feels cool to the touch and seemingly grasps at the blade hungrily. If Bautor is used to critically strike a creature that is bleeding, you may forgo dealing double damage and cast Vampiric Touch at 6th level on the target (DC 30 FORT). When invested, Bautor grants the ability to cast 4th level Pest Form as an innate occult spell at will, but only to turn into a bat.'
+},
+]
+console.log(magicItems);
