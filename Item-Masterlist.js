a34b977a069157e850d71d7825c78b50a6b335bb
MissaelOrtiz <81824706+MissaelOrtiz@users.noreply.github.com>
2021-04-13 17:33:49 -0700

    Added items
    Added ring of feather fall, garu's bomb ring, necrobane, and campfire bead.

--- Item-Masterlist.js
+++ Item-Masterlist.js
@@ -29,5 +29,45 @@ export const magicItems = [
   'rarity': 'Unique',
   'description': 'The entirety of the blade of this +2 Greater Striking Wounding Longsword is made of a crimson, polished crystalline material; its hilt made of cold iron that perpetually feels cool to the touch and seemingly grasps at the blade hungrily. If Bautor is used to critically strike a creature that is bleeding, you may forgo dealing double damage and cast Vampiric Touch at 6th level on the target (DC 30 FORT). When invested, Bautor grants the ability to cast 4th level Pest Form as an innate occult spell at will, but only to turn into a bat.'
 },
+ 
+{
+  'name':'Ring of Feather Fall',
+  'id': 3,
+  'type': 'ring',
+  'level': 3,
+  'traits : ['Abjuration', 'Invested', 'Magical'],
+  'rarity': 'Common',
+  'description': 'A polished silver ring with a feathered pattern engraved around all of its faces. Once a day, you are able to cast Feather Fall as an innate arcane spell.'
+},
+ 
+{
+  'name':`Garu's Bomb Ring`,
+  'id': 4,
+  'type': 'ring',
+  'level': 3,
+  'traits : ['Evocation', 'Fire', 'Magical'],
+  'rarity': 'Uncommon',
+  'description': 'A rough iron ring with a crude run inscribed onto a singular flat face. The ring remains warm to the touch regardless of temperature. The ring can be activated and thrown within 30 feet with two interact actions, dealing 2d8 fire damage to creatures in a 5 foot radius (DC 18 basic Reflex). The ring remains innert for one hour after being activated.'
+},
+ 
+{
+  'name':'Necrobane',
+  'id': 5,
+  'type': 'weapon',
+  'level': 3,
+  'traits : ['Magical', 'Necromancy'],
+  'rarity': 'Uncommon',
+  'description': 'An elegant and well balanced +1 magic bastard sword of elven make. Any hit with this sword deals an additional weapon damage dice against creatures with the undead trait, up to a maximum of three weapon damage dice.'
+},
+ 
+{
+  'name':'Campfire Bead',
+  'id': 6,
+  'type': 'necklace',
+  'level': '2',
+  'traits : ['Conjuration', 'Invested', 'Magical'],
+  'rarity': 'Common',
+  'description': 'A simple leather string holds a spherical fire opal which shifts like the flames of a fire. The campfire bead can be activated with two interact actions to create a small campfire in an empty space adjacent to you. This fire can be dismissed with a single action with the command trait. After 8 hours or being dismissed, whichever comes first, the fire disapears and the fire opal looses its color and cannot be activated again for twice the amount of time that the fire was active. This fire can be put out with sufficient effort, such as pouring a large amount of water onto it dismissing it.'
+},
 ]
 console.log(magicItems);
