ccb6c7003fcf6dd40ce7c9707f19e457c4543669
AngelDOrtiz <aortizghs@gmail.com>
2021-04-22 02:22:38 -0700

    Blue Eyes White Placeholder

--- app.js
+++ app.js
@@ -5,12 +5,36 @@ const button = document.getElementById('button')
 const span = document.getElementById('display');
 const randomResult = document.getElementById('randomResult')
 const rngButton = document.getElementById('button-rng');
+const itemCard = document.getElementById('item-card');
+const itemSquare = document.getElementById('item-square');
+
+
 
 //button.addEventListener('click', () => {
 //    span.textContent = getRing(magicItems);
 //})
+function removeSpin() {
+    itemCard.classList.remove('spin')
+}
+function addSpin() {
+    itemCard.classList.add('spin')
+}
 
 rngButton.addEventListener('click', () => {
     randomResult.textContent =  randomItem(magicItems);
+    
+    
+    itemCard.classList.remove('hidden')
+    
+
+    if (itemCard.classList.contains('spin')) {
+        setTimeout(removeSpin, 3000);
+    } 
+    if (itemCard.classList.length === 1) {
+        addSpin()
+        setTimeout(removeSpin, 3000)
+    }
+    // itemCard.classList.add('spin')
+    // itemCard.classList.remove('spin')
     
 })
